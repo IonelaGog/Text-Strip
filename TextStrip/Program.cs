ae052f945692ffd9c3c7a5b766b34c3fdcb0477d
Ionela - Maria Gog <gog_ionela@yahoo.com>
2020-06-06 00:18:03 +0300

    Add files via upload

---
+++ TextStrip/Program.cs
@@ -0,0 +1,22 @@
+﻿/*Gog Ionela-Maria
+ * Text strip problem
+*/
+
+using System;
+
+namespace TextStrip
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = "apples, pears # and bananas\ngrapes\nbananas !apples";
+            char[] array1 = {'#', '!'};
+
+            StripText var = new StripText();
+            string result = var.Text(text,array1);
+
+            Console.Write(result);
+        }
+    }
+}
---
+++ TextStrip/StripText.cs
@@ -0,0 +1,49 @@
+﻿/*Gog Ionela-Maria
+ * Text strip problem
+*/
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+using System.IO;
+
+namespace TextStrip
+{
+    class StripText
+    {
+        public StripText()
+        {
+        }
+
+        public string Text(string text, char[] mark)
+        {
+            int i, pos;
+            string trim = "";
+            string strip = "";
+            string text_new = "";
+            bool flag = false;
+
+            string[] sep = new string[] { "\n" };
+            string[] str = text.Split(sep, StringSplitOptions.None);
+
+            foreach (string s in str)
+            {
+                flag = false;
+                for (i = 0; i < mark.Length; i++)
+                {
+                    if (s.Contains(mark[i]))
+                    {
+                        pos = s.IndexOf(mark[i]);
+                        strip = s.Substring(0, s.Length - pos);
+                        trim = strip.Trim();
+                        text_new = text_new + trim + "\n";
+                        flag = true;
+                    }             
+                }
+                if (flag == false)
+                    text_new = text_new + s + "\n";
+            }      
+            return text_new;
+        }
+    }
+}
